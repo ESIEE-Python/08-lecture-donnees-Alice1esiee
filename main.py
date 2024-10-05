721142874dac42f33831b37adce98984ef2571b4
Alice LIN <alice.lin@edu.esiee.fr>
2024-10-05 17:48:15 +0000

    programme sur les fichiers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#### Imports et définition des variables globales
-import random
+"""Imports et définition des variables globales"""
+#import random
 
 FILENAME = "listes.csv"
 
@@ -15,38 +15,61 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    with open(filename, mode='r',encoding='utf8') as f:
+        for i in f:
+            new = []
+            nb = ""
+            for j in i.rstrip() :
+                if j != ';':
+                    nb += j
+                elif j == ';' :
+                    new.append(int(nb))
+                    nb = ""
+            if nb:
+                new.append(int(nb))
+            l.append(new)
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """Prend en argument la liste de listes 
+    retournée par read_data() et un indice k et retourne la kième liste"""
+    return data[k]
 
 def get_first(l):
-    return None
+    """Prend en argument une liste et retourne le premier élément de cette liste"""
+    return l[0]
 
 def get_last(l):
-    return None
+    """Prend en argument une liste et retourne le dernier élément de cette liste"""
+    return l[-1]
 
 def get_max(l):
-    return None
+    """Prend en argument une liste et retourne le maximum de cette liste"""
+    return max(l)
 
 def get_min(l):
-    return None
+    """Prend en argument une liste et retourne le minimum de cette liste"""
+    return min(l)
 
 def get_sum(l):
-    return None
+    """Prend en argument une liste et retourne la somme de cette liste"""
+    tot = 0
+    for i in l:
+        tot+= i
+    return tot
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    """Affiche la liste ligne par ligne et affiche la ligne 38"""
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
+
 
 
 if __name__ == "__main__":
